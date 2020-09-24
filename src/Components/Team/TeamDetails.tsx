c7c77eaa2eaab8af62574f7be918568cca7e300e
ItaiNatan <itai08425@gmail.com>
2020-09-24 21:56:02 +0300

    updates

--- src/Components/Team/TeamDetails.tsx
+++ src/Components/Team/TeamDetails.tsx
@@ -14,6 +14,7 @@ interface Props {
 const TeamDetails = (props: Props) => {
 
     const { team } = props
+    const { name, crestUrl, website, squad } = team
     const history = useHistory()
 
 
@@ -21,15 +22,15 @@ const TeamDetails = (props: Props) => {
     return (
         <div>
             <div className='flex-div'>
-                <h1> {team.name} </h1>
-                <img alt='' src={team.crestUrl} width="130px" height="68px" />
+                <h1> {name} </h1>
+                <img alt='' src={crestUrl} width="130px" height="68px" />
             </div>
             <div className='mb10'>
                 <b> founded: </b> &nbsp;
                 {team.founded ? team.founded : '---'}
             </div>
             <div className='mb10'>
-                <a href={team.website} target="_blank" rel="noopener noreferrer"> team website </a>
+                <a href={website} target="_blank" rel="noopener noreferrer"> team website </a>
             </div>
             <div className='mb10'>
                 <b> address: </b> &nbsp;
@@ -37,7 +38,7 @@ const TeamDetails = (props: Props) => {
             </div>
             <div>
                 <h3> Squad: </h3>
-                <SquadTable squad={team.squad} />
+                <SquadTable squad={squad} />
             </div>
             <button onClick={() => history.push('/teams/')}> back </button>
         </div>
